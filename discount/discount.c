40ba5f24e961e3650fc3742b14813aaf88d41285
Shafin <nasif.dnj@gmail.com>
2026-05-10 19:28:31 +0600

    updated discount.c

--- discount/discount.c
+++ discount/discount.c
@@ -4,7 +4,7 @@ float discount(float x){
 	if(x >= 500){
 		if(x >= 2000 && x <= 4000 && x != 2027)
 			return x * 0.05;
-		else if(x == 2027)
+		else if(x > 2026.99 && x < 2027.01)
 			return 270;
 		else if(x >= 5000){
 			if(x * 0.1 >= 2000){
@@ -27,7 +27,7 @@ float discount(float x){
 int main(){
 	float cart_value, cp, cp_to_tk;
 	float total_dis, payable, new_cp;
-	float x, y;
+	float remaining_cp, earned_cp, dis;
 	
 	printf("Enter the cart value: ");
 	scanf("%f", &cart_value);
@@ -35,18 +35,18 @@ int main(){
 	scanf("%f", &cp);
 
 	cp_to_tk = cp / 40;
-	
-	if(cart_value - discount(cart_value) >= cp_to_tk){
-		total_dis = cp_to_tk + discount(cart_value);
+	dis = discount(cart_value);
+	if(cart_value - dis >= cp_to_tk){
+		total_dis = cp_to_tk + dis;
 		payable = cart_value - total_dis;
 		new_cp = (cart_value / 10) * 4;
 	}
 	else{
 		payable = 0;
 		total_dis = cart_value;
-		x = (cp_to_tk - (cart_value - discount(cart_value))) * 40;
-		y = (cart_value / 10) * 4;
-		new_cp = x + y;
+		remaining_cp = (cp_to_tk - (cart_value - dis)) * 40;
+		earned_cp = (cart_value / 10) * 4;
+		new_cp = remaining_cp + earned_cp;
 	}
 
 	printf("Total discount: %.2f\nPayable: %.2f\nUpdated CP: %.2f\n", total_dis, payable, new_cp);
